95564c6e54db83a52a4cf53b1cc2fa95a59e4b6b
Shivangi Agrawal <48552129+shi-wal@users.noreply.github.com>
2025-06-19 10:24:44 +0530

    guided
    LeetCode POTD

---
+++ Arrays/2294. Partition Array Such That Maximum Difference Is K/code.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int cnt=1, start=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]-start>k){
+                cnt++;
+                start=nums[i];
+            }
+        }
+        return cnt;
+    }
+};
