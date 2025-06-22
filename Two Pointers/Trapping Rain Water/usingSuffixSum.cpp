3527b21a205c429c2ed8348ea6eb7c8f7a458f10
Shivangi Agrawal <48552129+shi-wal@users.noreply.github.com>
2025-06-22 11:30:25 +0530

    self

---
+++ Two Pointers/Trapping Rain Water/usingSuffixSum.cpp
@@ -0,0 +1,18 @@
+//TC: O(N), SC:O(N)
+
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        int n=arr.size();
+        int leftmax=0, total=0;
+        vector<int> suffixMax(n);
+        suffixMax[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--)
+            suffixMax[i]=max(suffixMax[i+1],arr[i]);
+        for(int i=0;i<n;i++){
+            leftmax=max(leftmax,arr[i]);
+            total+=min(leftmax,suffixMax[i])-arr[i];
+        }
+        return total;
+    }
+};
