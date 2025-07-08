eee151f2b26a0f8465fb03489bd6dea4b3fdb18c
Shivangi Agrawal <48552129+shi-wal@users.noreply.github.com>
2025-07-08 21:04:35 +0530

    myself - Medium

---
+++ Binary Tree/Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(!root) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        bool leftToRight=true;
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> level(size);
+            for(int i=0;i<size;i++){
+                TreeNode* node=q.front(); q.pop();
+                int index=(leftToRight)?i:(size-1-i);
+                level[index]=node->val;
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            leftToRight=!leftToRight;
+            res.push_back(level);
+        }
+        return res;
+    }
+};
