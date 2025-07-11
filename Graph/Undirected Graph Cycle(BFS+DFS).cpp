b107845d5d53acc29edc535ce0c9b647763e6d74
Shivangi Agrawal <48552129+shi-wal@users.noreply.github.com>
2025-07-11 17:06:21 +0530

    guided - medium

--- Graph/Undirected Graph Cycle(BFS+DFS).cpp
+++ Graph/Undirected Graph Cycle(BFS+DFS).cpp
@@ -1,3 +1,4 @@
+// Using BFS
 class Solution {
     bool detectCycle(int src, vector<vector<int>>& adjnode, vector<bool>& vis){
         vis[src]=true;
@@ -28,6 +29,35 @@ class Solution {
         vector<bool> vis(V,false);
         for(int i=0;i<V;i++){ // doing loop for checking if there are disconnected components
             if(!vis[i]) if(detectCycle(i,adjnode,vis)) return true;
+        }
+        return false;
+    }
+};
+
+
+// Using DFS
+class Solution {
+    bool dfs(int src, int parent, vector<vector<int>>& adjnode, vector<bool>& vis){
+        vis[src]=true;
+        for(auto adj : adjnode[src]){
+            if(!vis[adj]){
+                if(dfs(adj,src,adjnode,vis)) return true;
+                }
+            else if(parent!=adj) return true; // cycle detected
+        }
+        return false;
+    }
+    
+  public:
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> adjnode(V);
+        for(auto edge:edges){
+            adjnode[edge[0]].push_back(edge[1]);
+            adjnode[edge[1]].push_back(edge[0]);
+        }
+        vector<bool> vis(V,false);
+        for(int i=0;i<V;i++){ // doing loop for checking if there are disconnected components
+            if(!vis[i]) if(dfs(i,-1,adjnode,vis)) return true;
         }
         return false;
     }
