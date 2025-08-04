cb970a12d3f6eac767d63a33a6b691da3f82618a
Shivangi Agrawal <48552129+shi-wal@users.noreply.github.com>
2025-08-04 17:37:53 +0530

    myself - medium

---
+++ Dynamic Programming/198. House Robber.cpp
@@ -0,0 +1,35 @@
+// Top-Down
+
+class Solution {
+    int dorob(vector<int>& nums, vector<int>& dp, int i, int n){
+        if(i>=n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int take=nums[i]+dorob(nums,dp,i+2,n);
+        int nottake=dorob(nums,dp,i+1,n);
+        return dp[i]=max(take,nottake); 
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return dorob(nums,dp,0,n);
+    }
+};
+
+// Bottom-up
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int> dp(n,0);
+        dp[0]=nums[0]; dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int take=nums[i]+dp[i-2];
+            int nottake=dp[i-1];
+            dp[i]=max(take,nottake);
+        }
+        return dp[n-1];
+    }
+};
