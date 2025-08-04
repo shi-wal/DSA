007326864ecf7a4bcb73552920e43fe0ad23442f
Shivangi Agrawal <48552129+shi-wal@users.noreply.github.com>
2025-08-04 17:45:41 +0530

    myself - medium

--- Dynamic Programming/198. House Robber.cpp
+++ Dynamic Programming/198. House Robber.cpp
@@ -33,3 +33,18 @@ public:
         return dp[n-1];
     }
 };
+
+// TC: O(n), SC:O(1)
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev1=0, prev2=0;
+        for(int num:nums){
+            int temp=prev1;
+            prev1=max(prev2+num, prev1);
+            prev2=temp;
+        }
+        return prev1;
+    }
+};
