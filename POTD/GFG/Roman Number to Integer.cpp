39a6927be5570f343fd444a3d04da6e7c3ddf846
Shivangi Agrawal <48552129+shi-wal@users.noreply.github.com>
2025-08-06 18:57:54 +0530

    myself - easy - string

---
+++ POTD/GFG/Roman Number to Integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int romanToDecimal(string &s) {
+        unordered_map<char,int> mp;
+        mp['I']=1; mp['V']=5; mp['X']=10; mp['L']=50; mp['C']=100;
+        mp['D']=500; mp['M']=1000;
+        int n=s.length(), ans=0;
+        for(int i=0;i<n-1;i++){
+            if(mp[s[i]]<mp[s[i+1]]) ans-=mp[s[i]];
+            else ans+=mp[s[i]];
+        }
+        ans+=mp[s[n-1]];
+        return ans;
+    }
+};
