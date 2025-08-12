03a11af51e9e0bcb4d21cc37d8a0f171a8b75839
Shivangi Agrawal <48552129+shi-wal@users.noreply.github.com>
2025-08-12 07:38:37 +0530

    guided - medium - dp

---
+++ POTD/LeetCode/2787. Ways to Express an Integer as Sum of Powers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    const int mod=1000000007;
+public:
+    int numberOfWays(int n, int x) {
+        vector<int> powers;
+        for(int i=1; ; i++){
+            long long p=1;
+            for(int k=0;k<x;k++) p*=i;
+            if(p>n) break;
+            powers.push_back((int)p);
+        } 
+        vector<long long> dp(n+1,0);
+        dp[0]=1;
+        for(int p:powers){
+            for(int s=n;s>=p;s--){
+                dp[s]=(dp[s]+dp[s-p])%mod;
+            }
+        }
+        return (int)dp[n];
+    }
+};
