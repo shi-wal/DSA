3bbee6c0b2c3c713b1a5a66c8a2c3fc17e7dd0aa
Shivangi Agrawal <48552129+shi-wal@users.noreply.github.com>
2025-09-12 21:40:43 +0530

    guided - medium - greedy

---
+++ POTD/GFG/Minimize the Heights II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        int res=arr[n-1]-arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i]-k<0) continue;
+            int minH=min(arr[0]+k,arr[i]-k);
+            int maxH=max(arr[i-1]+k,arr[n-1]-k);
+            res=min(res,maxH-minH);
+        }
+        return res;
+    }
+};
